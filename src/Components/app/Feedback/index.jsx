8beac802dee7c9ddf04cf78831ebeb99d67615a8
ashimroyrupok <ashimroyrupok@gmail.com>
2024-04-20 18:25:00 +0600

    fix feedBack page responsive issue

--- src/Components/app/Feedback/index.jsx
+++ src/Components/app/Feedback/index.jsx
@@ -8,8 +8,8 @@ const Index = () => {
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
   // console.log(data)
   return (
-    <div className="bg-[#F5F5F5] py-6  ">
-      <div className="bg-white w-full shadow-md   mx-auto h-screen flex justify-center items-center">
+    <div className=" pt-6 md:pt-24 lg:pt-1 xl:pt-2 bg-white flex justify-center items-center h-full  ">
+      <div className=" w-full    mx-auto h-screen flex justify-center items-center">
         <div className=" w-full   ">
           <div className="text-center  flex justify-center items-center w-full">
             <Image
@@ -20,39 +20,39 @@ const Index = () => {
               alt="logo"
             />
           </div>
-          <p className="py-5 px-11 text-[#634F20] opacity-75 text-center text-xl ">
+          <p className="py-5 px-6 lg:px-11 text-[#634F20] opacity-75 text-center text-[17px] md:text-xl ">
             We sincerely regret that your experience did not meet your
             expectations. Your feedback is invaluable to us as it helps us to
             improve our services and ensure that all our guests enjoy their
             stay. Please fill out the form below with your contact details, and
             one of our team members will reach out to you promptly.
           </p>
-          <div className=" mx-auto text-[#634F20] w-[75%] md:w-1/2">
-            <div className="grid grid-cols-2 text-[#634F20] gap-4 py-2">
+          <div className=" mx-auto text-[#634F20] w-full px-8 lg:w-[75%] ">
+            <div className="grid grid-cols-1 md:grid-cols-2 text-[#634F20] gap-4 py-2">
               <input
                 type="text"
-                className="text-[#634F20] border-opacity-30 border rounded py-2 font-normal pl-2"
-                placeholder="Name"
+                className="text-[#634F20] border-opacity-30 border rounded py-3 font-normal pl-2"
+                placeholder="First name"
               />
               <input
-                type="number"
-                className="text-[#634F20] border-opacity-30 border py-2 rounded font-normal pl-2"
-                placeholder="Phone"
+                type="text"
+                className="text-[#634F20] border-opacity-30 border py-3 rounded font-normal pl-2"
+                placeholder="Last name"
               />
             </div>
             <input
-              type="mobile"
+              type="number"
               name=""
               id=""
-              placeholder="mobile"
-              className="text-[#634F20] border-opacity-30 border py-2 rounded w-full font-normal my-2 pl-2"
+              placeholder="Mobile"
+              className="text-[#634F20] border-opacity-30 border py-3 rounded w-full font-normal my-2 pl-2"
             />
             <input
               type="email"
               name=""
               id=""
-              placeholder="email"
-              className="text-[#634F20] border-opacity-30 border py-2 rounded w-full font-normal my-2 pl-2"
+              placeholder="Email"
+              className="text-[#634F20] border-opacity-30 border py-3 rounded w-full font-normal my-2 pl-2"
             />
             <div>
               {/* <Checkbox   /> */}
@@ -70,7 +70,7 @@ const Index = () => {
               </button>
             </div>
           </div>
-          <p className="py-5 text-center px-11 text-[#634F20] opacity-75 ">
+          <p className="pt-5 text-center px-5 md:px-11 text-[#634F20] opacity-75 ">
             We would appreciate the opportunity to discuss your experience
             further and see how we can make things right. Thank you for giving
             us the chance to address your concerns. If you would like to publish
