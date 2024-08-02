517b9cb761f9a82fd496040fe1ef03aba24d8ef2
shafi0072 <safi29317@gmail.com>
2024-08-02 19:07:08 +0600

    ec2

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://reviewoso.eu-south-1.elasticbeanstalk.com/:path*'
+        destination: 'http://18.102.51.122/:path*'
       }
     ];
   }
